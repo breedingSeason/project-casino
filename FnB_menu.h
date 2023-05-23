ac37a02500005d11542213a1e538f4b3e22d1e8c
breedingSeason <126639640+breedingSeason@users.noreply.github.com>
2023-05-23 23:36:27 +0930

    Add files via upload

--- FnB_menu.h
+++ FnB_menu.h
@@ -3,22 +3,22 @@
 #include <string>
 using namespace std;
 
-class FnB_menu{
-    protected:
-    string name;
-    int stock;//quantity that are in store
-    int price;
-    string type_food;
-    public:
-    FnB_menu();//default constructor
-    FnB_menu(string name,int stock, int price, string type);//constructor
-    string get_name();//get
-    int get_price();//get
-    int get_stock();//get
-    int get_type(); // returns an integer based on the type of food ordered
-    int prepare_food();//update stock every time a food item is ordered, decrease the stock by input x
-    bool change_stock(); //ensures the ordered food is in stock and the stock is not negative, return true if stock>=0
-    virtual int get_nutrition_info();//will return different values based on derived class
+class FnB_menu {
+ protected:
+  string name;
+  int stock;  // quantity that are in store
+  int price;
+  string type_food;
 
+ public:
+  FnB_menu();                                   // Default constructor
+  FnB_menu(string name, int stock, int price, string type);  // Parameterized constructor
+  string get_name();                            // Get name
+  int get_price();                              // Get price
+  int get_stock();                              // Get stock
+  int get_type();                               // Get type of FnB
+  int prepare_food();  // Update stock every time a food item is ordered
+  void change_stock();  // Ensures the ordered food is in stock and the stock is not negative, return true if stock>=0
+  virtual int get_nutrition_info();  // Will return different values based on derived class
 };
 #endif
