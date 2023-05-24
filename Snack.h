74e12bf6ea11c529b5ae1d1999d01802d81ea7a9
breedingSeason <126639640+breedingSeason@users.noreply.github.com>
2023-05-24 23:56:17 +0930

    Add files via upload

--- Snack.h
+++ Snack.h
@@ -2,17 +2,19 @@
 #define SNACK_H
 
 #include <string>
+
 #include "Food.h"
 
 using namespace std;
 
-class Snacks: public Food {
-    private:
-        string flavour;
-    public:
-    Snacks(); // default constructor
-    Snacks(string name,int stock, int price, string type, int calories, string flavour); // 
-    string get_flavour() override; // returns the flavour
+class Snacks : public Food {
+ private:
+  string flavour;
+
+ public:
+  Snacks();  // Default constructor
+  Snacks(string name, int stock, int price, string type, int calories, string flavour);         // Parameterized constructor
+  string get_flavour() override;  // Returns flavour
 };
 
 #endif
