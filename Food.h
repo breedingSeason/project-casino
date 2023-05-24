8e24261fff324c6606aabf5f2af1d6bf9264b035
breedingSeason <126639640+breedingSeason@users.noreply.github.com>
2023-05-24 23:53:19 +0930

    Add files via upload

--- Food.h
+++ Food.h
@@ -1,14 +1,18 @@
 #ifndef FOOD_H
 #define FOOD_H
 #include <string>
+
 #include "FnB_menu.h"
 using namespace std;
-class Food: public FnB_menu{
-    protected:
-        int calories;
-    public:
-        Food();//default cons
-        Food(string name,int stock, int price, string type, int calories);//cons
-        int get_nutrition_info() override; //returns number of calories
+
+class Food : public FnB_menu {
+ protected:
+  int calories;
+
+ public:
+  Food(); // Default constructor
+  Food(string name, int stock, int price, string type, int calories); // Parameterized constructor
+  int get_nutrition_info() override;  // Returns calories
 };
+
 #endif
