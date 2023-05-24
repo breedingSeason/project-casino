31c0103ab436d0a6f9f0035d40294313a950966f
breedingSeason <126639640+breedingSeason@users.noreply.github.com>
2023-05-25 03:23:10 +0930

    Add files via upload

--- Hard_Game.h
+++ Hard_Game.h
@@ -14,12 +14,13 @@ class Hard_Game : public Games {
   int Player_cards_Sum;
 
  public:
-  Hard_Game(); // Default constructor
-  Hard_Game(int bet_amount, int num_players); // Parameterized constructor
-  int get_max_players() override; // // Returns the maximum number of players allowed
-  void get_card() override; // Get card
-  void set_num_players(int x) override; // Set number of players
-  bool won_game() override; // Return true if the player win the game
+  Hard_Game();                                 // Default constructor
+  Hard_Game(int bet_amount, int num_players);  // Parameterized constructor
+  int get_max_players()
+      override;              // // Returns the maximum number of players allowed
+  void get_card() override;  // Get card
+  void set_num_players(int x) override;  // Set number of players
+  bool won_game() override;  // Return true if the player win the game
 };
 
 #endif
